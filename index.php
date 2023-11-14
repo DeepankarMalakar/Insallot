08de17d949efd91b9121606d938855bd2c3554c0
DeepankarMalakar <deepankarmalakar007@gmail.com>
2023-11-14 17:25:33 +0530

    Landing page

--- index.php
+++ index.php
@@ -8,14 +8,70 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css2?family=Merienda:wght@400;700&family=Poppins:wght@400;600&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
-
+    <link rel="stylesheet" href="style.css">
     <style>
         * {
             font-family: 'Poppins', sans-serif;
         }
 
-        .h-font {
+        .h-font,
+        #Big-text-2 {
             font-family: 'Merienda', cursive;
+        }
+
+        #Big-text {
+            font-size: 40px;
+            font-family: 'Merienda';
+        }
+
+        #Big-text-2 {
+            font-size: 45px;
+        }
+
+        #front-content {
+            width: 100%;
+            height: 100vh;
+            background-image: url("realistic-white-golden-geometric-background_79603-2032.avif");
+            background-position: center;
+            background-size: cover;
+            position: absolute;
+        }
+
+        #content-heading {
+            font-size: 70px;
+            font-weight: 700;
+            line-height: 100px;
+            margin-bottom: 10px;
+        }
+
+        .text-box {
+            margin-top: 14%;
+            margin-left: 50px;
+        }
+
+        #content-para,
+        #content-span {
+            font-size: 20px;
+            color: #000;
+        }
+
+        .text-box span {
+            color: blue;
+        }
+
+        #content-btn {
+            margin-top: 18px;
+            outline: none;
+            border: none;
+            font-size: 16px;
+            background-color: #000;
+            color: #fff;
+            padding: 14px;
+            border-radius: 5px;
+        }
+
+        #content-btn:hover {
+            background-color: blue;
         }
     </style>
 </head>
@@ -54,6 +110,18 @@
             </div>
         </div>
     </nav>
+    <div class="container-fluid" id="front-content">
+        <div class="text-box">
+            <h1 id="content-heading"><span>Welcome</span> to Insallot !</h1>
+            <p id="content-para">Simplify classroom management effortlessly .</p>
+            <span id="content-span">Streamline your Classrooms with ease.</span>
+            <br>
+            <span id="content-span">Join now</span>
+            <br>
+            <button id="content-btn">Get Started</button>
+        </div>
+    </div>
+
 
     <div class="modal fade" id="loginModel" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
         <div class="modal-dialog">
@@ -87,7 +155,7 @@
     <div class="modal fade" id="SignInModel" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
         <div class="modal-dialog modal-lg">
             <div class="modal-content">
-            <span class="badge bg-light text-primary mb-3 text-wrap lh-base mt-2 fs-6">NOTE: We will never share your credentials with others.</span>
+                <span class="badge bg-light text-primary mb-3 text-wrap lh-base mt-2 fs-6">NOTE: We will never share your credentials with others.</span>
                 <form>
                     <div class="modal-header">
                         <h5 class="modal-title d-flex align-items-center" id="staticBackdropLabel"><i class="bi bi-person-add fs-2 me-2"></i> Sign Up </h5>
@@ -134,7 +202,7 @@
                                 <div class="col-md-6">
                                     <div class="mb-4">
                                         <label class="form-label">Address:</label>
-                                        <textarea class="form-control"rows="1"></textarea>
+                                        <textarea class="form-control" rows="1"></textarea>
                                     </div>
                                 </div>
 
@@ -145,7 +213,7 @@
                                     </div>
                                 </div>
                                 <div class="text-center my-1">
-                                <button class="btn btn-dark shadow-none" type="submit">Submit</button>
+                                    <button class="btn btn-dark shadow-none" type="submit">Submit</button>
                                 </div>
                             </div>
                             <!-- <div class="mb-3">
@@ -168,6 +236,7 @@
             </div>
         </div>
     </div>
+
 
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
